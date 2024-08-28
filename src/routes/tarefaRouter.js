bac6cad93147b7a4d95b9d3eac9904e059100bc0
Arthur Leony <leonyalvesarthuroliveira263@gmail.com>
2024-08-28 07:49:03 -0300

    RF01 finalizado

--- src/routes/tarefaRouter.js
+++ src/routes/tarefaRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getAll, create } from "../controllers/tarefaController.js";
+import { /*getAll*/ create } from "../controllers/tarefaController.js";
 
 const router = Router();
 
-router.get("/", getAll);
-router.post("/", create)
+// router.get("/", getAll);
+router.post("/", create);
 
 export default router;
